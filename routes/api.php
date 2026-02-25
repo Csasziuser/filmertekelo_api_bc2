2e56b30312d137edf4e5e580af4f7d56ad22be02
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-25 12:25:09 +0100

    elkeszult utvonalak controllerek

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\RatingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/movies', [MovieController::class, "index"]);
+Route::post('/movies', [MovieController::class, "store"]);
+
+Route::get('/ratings', [RatingController::class, "index"]);
+Route::post('/ratings', [RatingController::class, "store"]);
