1c89e9382d4d11b5973174bb64cce53d642a1257
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-25 12:33:59 +0100

    Kész a movie post

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
@@ -19,7 +20,30 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "title" => "required|string|min:1",
+            "length"=>"required|integer|min:1",
+            "genre"=>"required|string|min:1",
+        ],[
+            "string" => ":attribute nak szövegnek kell lennie.",
+            "integer" => ":attribute nak számnak kell lennie.",
+            "required" => ":attribute kötelező mező.",
+            "title.min" => ":attribute nak minimum ennyinek kell lennie: :min.",
+            "length.min" => ":attribute nak minimum ennyinek kell lennie: :min.",
+            "genre.min" => ":attribute nak minimum ennyinek kell lennie: :min."
+        ],[
+            "title" => "Cím",
+            "length" => "Hossz",
+            "genre"=> "Műfaj"
+        ]);
+        try{
+            $movie = Movie::create($validated);
+            return response()->json(["message"=> "Sikeres feltöltés"],201,options:JSON_UNESCAPED_UNICODE);
+        } catch(\Exception $e)
+        {
+            return response()->json(["message"=> "Sikertelen feltöltés"],404,options:JSON_UNESCAPED_UNICODE);
+        
+        }
     }
 
     /**
